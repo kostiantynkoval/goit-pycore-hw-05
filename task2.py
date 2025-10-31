6df6de6aa1c1b214f5f9a6f4ace19a0bebf4e006
Kostiantyn Koval <kkoval@densify.com>
2025-10-31 09:30:59 -0400

    KovalK: updated regexp

--- task2.py
+++ task2.py
@@ -1,7 +1,7 @@
 from re import finditer
 
 def generator_numbers(text):
-    for match in finditer(r" \d+.\d+ ", text):
+    for match in finditer(r" \d+\.\d+ | \d+ ", text):
         yield float(match.group())
 
 def sum_profit(text, generator_function):
@@ -9,5 +9,6 @@ def sum_profit(text, generator_function):
 
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 та 324.00 доларів."
-total_income = sum_profit(text, generator_numbers)
+text1 = "Пункт 28а: Загальний дохід працівника складається з декількох частин: 1000 як основний дохід, доповнений додатковими надходженнями 27.45555555555555555 та 324.000000000000001 доларів."
+total_income = sum_profit(text1, generator_numbers)
 print(f"Загальний дохід: {total_income:.2f}")
